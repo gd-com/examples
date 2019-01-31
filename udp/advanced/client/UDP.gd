ed58d38016f4a9d8132af29438379700c141d621
Tilican <titben047@hotmail.fr>
2019-01-31 22:11:20 +0100

    Advanced example UDP

---
+++ udp/advanced/client/UDP.gd
@@ -0,0 +1,42 @@
+extends Node
+
+var connection = PacketPeerUDP.new()
+
+var ADDRESS = '127.0.0.1'
+var PORT_SERVER = 9091
+var PORT_CLIENT = 9092
+
+func _ready():
+	print("Start client UDP")
+	# Connect
+	connection.set_dest_address(ADDRESS, PORT_SERVER)
+	connection.listen(PORT_CLIENT, ADDRESS)
+
+func _process(delta):
+	if connection.is_listening():
+        if Input.is_action_just_released("ui_left"):
+            var buffer = StreamPeerBuffer.new()
+            buffer.put_u16(1001)
+            connection.put_packet(buffer.get_data_array())
+
+        if Input.is_action_just_released("ui_right"):
+            var buffer = StreamPeerBuffer.new()
+            buffer.put_u16(1002)
+            print('pass')
+            connection.put_packet(buffer.get_data_array())
+        
+        if connection.get_available_packet_count() > 0 :
+            var packet = connection.get_packet()
+            var buffer = StreamPeerBuffer.new()
+            buffer.set_data_array(packet)
+            
+            var type = buffer.get_u16()
+            print('Recieve %s' % type)
+            match type:
+                1:
+                    var length = buffer.get_u32()
+                    print("My id is %s !" % buffer.get_string(length))
+                1003:
+                    print("We recieve OK_GO_LEFT !")
+                1004:
+                    print("We recieve OK_GO_RIGHT !")
---
+++ udp/advanced/server/src/index.js
@@ -0,0 +1,36 @@
+const dgram = require('dgram')
+const { GdBuffer } = require('@gd-com/utils')
+const { v4 } = require('uuid')
+const process = require('./process')
+
+const server = dgram.createSocket('udp4')
+
+let clients = {}
+
+server.on('listening', () => {
+  let address = server.address()
+  console.log(`UDP Server listening on ${address.address}:${address.port}`)
+})
+
+server.on('message', async (buf, remote) => {
+  let client = null
+  if (!clients.hasOwnProperty(`${remote.address}-${remote.port}`)) {
+    clients[`${remote.address}-${remote.port}`] = {uuid: v4()}
+    client = clients[`${remote.address}-${remote.port}`]
+    console.log(`[${client.uuid}] New client from ${remote.address}:${remote.port}`)
+  } else {
+    client = clients[`${remote.address}-${remote.port}`]
+  }
+
+  let recieve = new GdBuffer(Buffer.from(buf))
+  const type = await recieve.getU16()
+
+  console.log(`[${client.uuid}] << Recieve packet code`, type)
+  if (process.hasOwnProperty(type)) {
+    process[`${type}`](client, server, remote, recieve.getBuffer())
+  } else {
+    console.log(`[${client.uuid}] << Unknow packet code`, type)
+  }
+})
+
+server.bind(9091, '127.0.0.1')
